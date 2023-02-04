389a966c0552e96bfaa4aad2ace301e9db523cc2
vikash <vikash31dec@hotmail.com>
2023-02-05 00:48:00 +0530

    First commit

--- UserService/src/main/java/com/lcwd/user/service/exceptions/GolbalExceptionHandler.java
+++ UserService/src/main/java/com/lcwd/user/service/exceptions/GolbalExceptionHandler.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GolbalExceptionHandler {
 	
-	public ResponceEntity<ApiResponse> handlerRequestNotFoundException(){
-		
-	}
+//	public ResponceEntity<ApiResponse> handlerRequestNotFoundException(){
+//		
+//	}
 
 }
